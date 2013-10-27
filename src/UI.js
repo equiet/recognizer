fa1252ef41905e7ed5c7c0f66a2b707c42c91dba
Jakub Jurových <jakub.jurovych@gmail.com>
2013-10-27 17:00:43 +0100

    UI updates, icons

--- src/UI.js
+++ src/UI.js
@@ -1,62 +1,83 @@
 define(function (require, exports, module) {
-    'use strict';
+    'use strict'
 
     exports.statusIndicator = (function() {
 
         var $indicators = $('#status-indicators'),
-            $indicator = $('<div id="status-indicator-recognizer" />');
+            $indicator = $('<div id="status-indicator-recognizer" />')
 
         // Append into Status indicators panel, before spinner
-        $indicators.append($indicator);
-        $indicators.find('.spinner').remove().appendTo($indicators);
+        $indicators.append($indicator)
+        $indicators.find('.spinner').remove().appendTo($indicators)
 
-        var statusActions = {};
+        var statusActions = {}
 
         // Error
         statusActions[-1] = function() {
-            $indicator.attr('class', 'is-error');
-            $indicator.html('Error');
+            $indicator.attr('class', 'is-error')
+            $indicator.html('Error')
         };
 
         // Incative
         statusActions[0] = function() {
-            $indicator.attr('class', 'is-inactive');
-            $indicator.html('Inactive');
+            $indicator.attr('class', 'is-inactive')
+            $indicator.html('Inactive')
 
         };
 
         // Connecting to the remote debugger
         statusActions[1] = function() {
-            $indicator.attr('class', 'is-connecting');
-            $indicator.html('Connecting');
+            $indicator.attr('class', 'is-connecting')
+            $indicator.html('Connecting')
 
         };
 
         // Loading agents
         statusActions[2] = function() {
-            $indicator.attr('class', 'is-loading-agents');
-            $indicator.html('Loading agents');
+            $indicator.attr('class', 'is-loading-agents')
+            $indicator.html('Loading agents')
 
         };
 
         // Active
         statusActions[3] = function() {
-            $indicator.attr('class', 'is-active');
-            $indicator.html('Active');
+            $indicator.attr('class', 'is-active')
+            $indicator.html('Active')
         };
 
         // Out of sync
         statusActions[4] = function() {
-            $indicator.attr('class', 'is-out-of-sync');
-            $indicator.html('Out of sync');
+            $indicator.attr('class', 'is-out-of-sync')
+            $indicator.html('Out of sync')
         };
 
         return {
-            updateIndicator: function(code) {
-                statusActions[code]();
+            updateStatus: function(code) {
+                statusActions[code]()
             }
         };
 
-    }());
+    }())
 
-});
+    exports.panel = (function() {
+
+        var Resizer = brackets.getModule('utils/Resizer')
+
+        var $panel = $('<div id="recognizer-panel" />'),
+            $mainToolbar = $('#main-toolbar'),
+            $content = $('.main-view .content')
+
+        $panel.html(require('text!src/panel.html'));
+        $mainToolbar.parent().append($panel)
+
+        Resizer.makeResizable($panel, Resizer.DIRECTION_HORIZONTAL, Resizer.POSITION_LEFT, 100, false, false)
+
+        $panel.on('panelResizeUpdate', function() {
+            $content.css('right', $panel.width() + $mainToolbar.width())
+        })
+        $panel.trigger('panelResizeUpdate');
+
+
+    }())
+
+})
