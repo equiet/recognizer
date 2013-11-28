cc5054f28da50f44382ea99ec07a4e3ed309246f
Jakub Jurových <jakub.jurovych@gmail.com>
2013-11-28 02:58:51 +0100

    Remove unused code

--- main.js
+++ main.js
@@ -16,8 +16,6 @@ define(function (require, exports, module) {
         UI = require('src/UI'),
         WidgetManager = require('src/WidgetManager');
 
-    var _logHandle;
-
 
     ExtensionUtils.loadStyleSheet(module, 'main.less');
     ExtensionUtils.loadStyleSheet(module, 'src/styles/font-awesome.css');
@@ -37,11 +35,6 @@ define(function (require, exports, module) {
 
 
 
-
-    var _tracerObjectId;
-    var _theseusObjectId;
-
-
     var _loggedNodes = [], _loggedEventNames = [], _loggingExceptions = false, _loggingConsoleLogs = false;
     var _logHandle;
 
@@ -49,77 +42,6 @@ define(function (require, exports, module) {
     // Insert tracer into browser
     $(LiveDevelopment).on('statusChange', function(e, status) {
         if (status === 3 ) {
-
-            // Inspector.Runtime.evaluate('__recognizer.connect()', function (res) {
-
-            //     if (!res.wasThrown) {
-            //         _tracerObjectId = res.result.objectId;
-            //         console.log('[recognizer] tracer retrieved', _tracerObjectId, res.result);
-
-            //         Inspector.Runtime.callFunctionOn(_tracerObjectId, '__recognizer.test', function (res) {
-            //             console.log('[recognizer] test function called, expect confirmation', res);
-            //         });
-
-            //         setInterval(function () {
-
-            //             Inspector.Runtime.callFunctionOn(_tracerObjectId, '__recognizer.getCalls', function (res) {
-
-            //                 var args = JSON.parse(res.result.value);
-            //                 args.forEach(function (val, index) {
-
-            //                     var d = new Date();
-
-            //                     if (!inlineWidgets[val.line]) {
-
-            //                         inlineWidgets[val.line] = new DebugInlineWidget();
-
-            //                         inlineWidgets[val.line].load(EditorManager.getCurrentFullEditor());
-
-            //                         hostEditor.addInlineWidget({line: val.line, ch: 20}, inlineWidgets[val.line], true).done(function () {
-            //                             inlineWidgets[val.line].addRow(inlineWidgets[val.line].$lastGroup, d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds(), d.getMilliseconds(), val.args);
-            //                         });
-            //                     } else {
-            //                         inlineWidgets[val.line].addRow(inlineWidgets[val.line].$lastGroup, d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds(), d.getMilliseconds(), val.args);
-            //                     }
-
-            //                 });
-            //             });
-
-            //         }, 100);
-
-
-            //     } else {
-            //         console.log('[recognizer] failed to retrieve tracer', res);
-            //     }
-
-            // });
-            //
-
-
-
-            // Inspector.Runtime.evaluate("__tracer.connect()", function (res) {
-            //     if (!res.wasThrown) {
-            //         _theseusObjectId = res.result.objectId;
-
-            //         Inspector.Runtime.callFunctionOn(_theseusObjectId, "__tracer.trackNodes", [], true, true, function (res) {
-
-            //             var id = setInterval(function () {
-            //                 Inspector.Runtime.callFunctionOn(_theseusObjectId, "__tracer.newNodes", [/*handle*/], function (res) {
-            //                     console.log(res);
-            //                     // if (nodes) {
-            //                     //     _addNodes(nodes);
-            //                     // }
-            //                 });
-            //             }, 1000);
-
-            //         });
-
-            //     } else {
-            //         console.log("failed to get tracer instance", res);
-            //     }
-            // });
-
-
         }
     });
 
@@ -221,25 +143,6 @@ define(function (require, exports, module) {
             });
         }, 2000);
 
-
-
-        // setInterval(function() {
-        //     if (Agent.isReady()) {
-
-        //         Agent.trackLogs({
-        //             ids: _loggedNodes,
-        //             eventNames: _loggedEventNames,
-        //             exceptions: _loggingExceptions,
-        //             logs: _loggingConsoleLogs,
-        //         }, function (handle) {
-        //             _logHandle = handle;
-        //             console.log('handle', handle);
-        //             console.log(_loggedNodes);
-        //             console.log(_loggedEventNames);
-        //         });
-
-        //     }
-        // }, 1000);
 
 
     }
