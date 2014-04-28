d059721f31409b92ac70b0e9f59919694e7cee5e
Jakub Jurových <jakub.jurovych@gmail.com>
2014-04-28 19:57:13 +0200

    Add instrumentation tests

--- unittests.js
+++ unittests.js
@@ -6,8 +6,100 @@ define(function (require, exports, module) {
 
     describe('Instrumentation', function() {
 
-        it('should return inspectableObjects', function() {
-//            var instrumentedCode = Instrumenter.instrument('function() {}');
+        it('should find the position of simple MemberExpression probes in the code', function() {
+
+            // Simple MemberExpression
+            var instrumentedCode = Instrumenter.instrument('object.property;');
+
+            expect(instrumentedCode.probes.length).toBe(2);
+            
+            // 'object'
+            expect(instrumentedCode.probes[0].loc.start.line).toBe(1);
+            expect(instrumentedCode.probes[0].loc.end.line).toBe(1);
+            expect(instrumentedCode.probes[0].loc.start.column).toBe(0);
+            expect(instrumentedCode.probes[0].loc.end.column).toBe(6);
+
+            // 'property'
+            expect(instrumentedCode.probes[1].loc.start.line).toBe(1);
+            expect(instrumentedCode.probes[1].loc.end.line).toBe(1);
+            expect(instrumentedCode.probes[1].loc.start.column).toBe(7);
+            expect(instrumentedCode.probes[1].loc.end.column).toBe(15);
+
+        });
+
+        it('should find the position of nested MemberExpression probes in the code', function() {
+
+            // Simple MemberExpression
+            var instrumentedCode = Instrumenter.instrument('object.property.subproperty;');
+
+            expect(instrumentedCode.probes.length).toBe(3);
+            
+            // 'object'
+            expect(instrumentedCode.probes[0].loc.start.line).toBe(1);
+            expect(instrumentedCode.probes[0].loc.end.line).toBe(1);
+            expect(instrumentedCode.probes[0].loc.start.column).toBe(0);
+            expect(instrumentedCode.probes[0].loc.end.column).toBe(6);
+
+            // 'property'
+            expect(instrumentedCode.probes[1].loc.start.line).toBe(1);
+            expect(instrumentedCode.probes[1].loc.end.line).toBe(1);
+            expect(instrumentedCode.probes[1].loc.start.column).toBe(7);
+            expect(instrumentedCode.probes[1].loc.end.column).toBe(15);
+
+            // 'subproperty'
+            expect(instrumentedCode.probes[2].loc.start.line).toBe(1);
+            expect(instrumentedCode.probes[2].loc.end.line).toBe(1);
+            expect(instrumentedCode.probes[2].loc.start.column).toBe(16);
+            expect(instrumentedCode.probes[2].loc.end.column).toBe(27);
+
+        });
+
+        it('should find the position of CallExpression', function() {
+
+            // Simple MemberExpression
+            var instrumentedCode = Instrumenter.instrument('fn();');
+
+            expect(instrumentedCode.probes.length).toBe(2);
+
+            // 'fn'
+            expect(instrumentedCode.probes[0].loc.start.line).toBe(1);
+            expect(instrumentedCode.probes[0].loc.end.line).toBe(1);
+            expect(instrumentedCode.probes[0].loc.start.column).toBe(0);
+            expect(instrumentedCode.probes[0].loc.end.column).toBe(2);
+
+            // 'fn()'
+            expect(instrumentedCode.probes[1].loc.start.line).toBe(1);
+            expect(instrumentedCode.probes[1].loc.end.line).toBe(1);
+            expect(instrumentedCode.probes[1].loc.start.column).toBe(0);
+            expect(instrumentedCode.probes[1].loc.end.column).toBe(4);
+
+        });
+
+        it('should find the position of CallExpression with MemberExpression', function() {
+
+            // Simple MemberExpression
+            var instrumentedCode = Instrumenter.instrument('object.property();');
+
+            expect(instrumentedCode.probes.length).toBe(3);
+
+            // 'object'
+            expect(instrumentedCode.probes[0].loc.start.line).toBe(1);
+            expect(instrumentedCode.probes[0].loc.end.line).toBe(1);
+            expect(instrumentedCode.probes[0].loc.start.column).toBe(0);
+            expect(instrumentedCode.probes[0].loc.end.column).toBe(6);
+
+            // 'property'
+            expect(instrumentedCode.probes[1].loc.start.line).toBe(1);
+            expect(instrumentedCode.probes[1].loc.end.line).toBe(1);
+            expect(instrumentedCode.probes[1].loc.start.column).toBe(7);
+            expect(instrumentedCode.probes[1].loc.end.column).toBe(15);
+
+            // 'object.property()'
+            expect(instrumentedCode.probes[2].loc.start.line).toBe(1);
+            expect(instrumentedCode.probes[2].loc.end.line).toBe(1);
+            expect(instrumentedCode.probes[2].loc.start.column).toBe(0);
+            expect(instrumentedCode.probes[2].loc.end.column).toBe(17);
+
         });
 
     });
